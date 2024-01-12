537130f12de298606f8a51f59b6db0fbe58b6a84
Ichi Hosotsuji <hoshos@IchinoMacBook-Air.local>
2024-01-13 01:07:28 +0900

    comment

--- src/app/page.js
+++ src/app/page.js
@@ -22,11 +22,11 @@ export default function Home() {
       <Image src={kyotouImage} alt="kyotouniv" className={styles.kyotou} />
       <Image src={sumitomoImage} alt="sumitomo" className={styles.sumitomo} />
       <Image src={lignoImage} alt="lignoFM" className={styles.lignoFM} />
-      <button type="button" onClick={() => router.push("./members")} className={styles.members}>Members</button>
-      <button type="button" onClick={() => router.push("./amateur")} className={styles.amateur}>Amateur<br/>Mission</button>
-      <button type="button" onClick={() => router.push("./about")} className={styles.about}>About<br/>LignoSat</button>
+      <button type="button" onClick={() => router.push("./members")} className={styles.members}>Members  </button>
+      <button type="button" onClick={() => router.push("./amateur")} className={styles.amateur}>Amateur <br/>Mission</button>
+      <button type="button" onClick={() => router.push("./about")} className={styles.about}>About<br/>LignoSat  </button>
       <button type="button" onClick={() => router.push("./gallery")} className={styles.gallery}>Gallery</button>
-      <button type="button" onClick={() => router.push("/contact")} className={styles.contact}>Contact</button>
+      <button type="button" onClick={() => router.push("/contact")} className={styles.contact}>Contact </button>
     </section>
   </main>
   </>
